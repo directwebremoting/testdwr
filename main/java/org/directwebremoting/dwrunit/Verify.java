dc94b630296d6a00b6902cd65de1c662b106f292
Joe Walker <joe at getahead dot org>
2008-11-24 15:44:39 +0000

    Verify is globally useful and the plain Test class had some simplification Make the order of the expected and actual variables consistent in equals methods

--- main/java/org/directwebremoting/dwrunit/Verify.java
+++ main/java/org/directwebremoting/dwrunit/Verify.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.testdwr.plain;
+package org.directwebremoting.dwrunit;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
