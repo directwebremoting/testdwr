67dfa3bd0b3b8cddb1e934954cb4c53c571f9823
xmaniac <xmaniac@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2009-02-18 15:16:43 +0000

    Just a little bit more complex DWR-343 test

--- testdwr/trunk/main/java/org/testdwr/convert/generics/Service.java
+++ testdwr/trunk/main/java/org/testdwr/convert/generics/Service.java
@@ -1,8 +1,8 @@
 package org.testdwr.convert.generics;
 
-public interface Service<T extends Command>
+public interface Service<E extends Command, T extends Command>
 {
 
-    Object execute(T command);
+    Object execute(int i, Double d, E command, T other);
 
 }
