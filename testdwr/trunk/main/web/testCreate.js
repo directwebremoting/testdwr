cd8ff16aaef39207b383cb23b682b06e9b7770a9
davidmarginian <davidmarginian@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2009-05-14 00:57:40 +0000

    DWR-365 - Ensure all tests from testdwr.war are passing.

---
+++ testdwr/trunk/main/web/testCreate.js
@@ -0,0 +1,35 @@
+createTestGroup("Create");
+
+window.testCreateStaticWithGetInstanceMethod = function() {
+  SingletonGetInstance.disallowed({
+    callback:function(data) {
+      verifyNull(data);
+    },
+    exceptionHandler:createDelayed(function(message, ex) {
+      verifyEqual(message, ex.message);
+      // What's thrown is a org.directwebremoting.ConversionException, but that
+      // isn't mapped so the type we see is just the basic java.lang.Throwable
+      verifyEqual(ex.javaClassName, "java.lang.Throwable");
+    })
+  });
+  SingletonGetInstance.allowed(createOptions(function (data) {
+    verifyEqual(data, "SUCCESS");
+  }));
+};
+
+window.testCreateStaticWithCustomInstanceMethod = function() {
+  SingletonCustomMethodName.disallowed({
+    callback:function(data) {
+      verifyNull(data);
+    },
+    exceptionHandler:createDelayed(function(message, ex) {
+      verifyEqual(message, ex.message);
+      // What's thrown is a org.directwebremoting.ConversionException, but that
+      // isn't mapped so the type we see is just the basic java.lang.Throwable
+      verifyEqual(ex.javaClassName, "java.lang.Throwable");
+    })
+  });
+  SingletonCustomMethodName.allowed(createOptions(function (data) {
+    verifyEqual(data, "SUCCESS");
+  }));
+};
