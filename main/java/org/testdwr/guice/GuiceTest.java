915548dbad8c059aac7fe10ec7b81611caaf8cc2
mikewse <mikewse@users.noreply.github.com>
2010-11-30 09:32:46 +0000

    Updates for versioning changes

--- main/java/org/testdwr/guice/GuiceTest.java
+++ main/java/org/testdwr/guice/GuiceTest.java
@@ -32,7 +32,6 @@ import org.directwebremoting.util.VersionUtil;
  */
 public class GuiceTest
 {
-    @SuppressWarnings("deprecation")
     public Verify checkContext(String contextPath)
     {
         ServerContext serverContext = ServerContextFactory.get();
@@ -40,7 +39,7 @@ public class GuiceTest
         Verify verify = new Verify();
 
         verify.equals("ContextPath", contextPath, serverContext.getContextPath());
-        verify.equals("Version", VersionUtil.getVersion(), serverContext.getVersion());
+        verify.equals("Version", VersionUtil.getLabel(), serverContext.getVersion());
         verify.equals("Container.class", DefaultContainer.class.getName(), container.getClass().getName());
         verify.equals("Container.getBean", "DwrGuiceServlet", container.getBean("ContainerType"));
 
