8bb2cb036729bd07eb7f1cc1661b0545c1211ae6
mikewse <mikewse@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2013-03-07 16:21:11 +0000

    removing incorrect memory leak test

--- testdwr/trunk/main/web/testAdvanced.js
+++ testdwr/trunk/main/web/testAdvanced.js
@@ -1,26 +1,6 @@
 
 createTestGroup("Advanced");
 
-/**
- *
- */
-window.testAdvancedMemoryLeaks = function(count) {
-  if (count == null) {
-    count = 0;
-  }
-  if (count > 10000) {
-    success("testMemoryLeaks");
-  }
-  else {
-    var next = count + 1;
-    Test.doNothing(function() {
-      setTimeout(function() {
-        testAdvancedMemoryLeaks(next);
-      }, 0);
-    });
-  }
-};
-
 window.testAdvancedGlobalRedirectResponse = function() {
 	var c = new dwrunit.SingleAsyncCompletor;
 	dwr.engine.setTextOrRedirectHandler(waitAsync(c, function(data) {
