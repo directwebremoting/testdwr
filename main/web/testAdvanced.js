1dc652749aec41f6966faecf42f7e1675e38c8f5
David Marginian <dmarginian@users.noreply.github.com>
2013-03-13 02:15:47 +0000

    Comment textHtmlResponse tests.

--- main/web/testAdvanced.js
+++ main/web/testAdvanced.js
@@ -1,6 +1,29 @@
 
 createTestGroup("Advanced");
 
+window.testAdvancedGlobalRedirectResponse = function() {
+    var c = new dwrunit.SingleAsyncCompletor;
+    dwr.engine.setTextOrRedirectHandler(waitAsync(c, function(data) {
+        verifyEqual(302, data.status);
+    }));
+    Test.testRedirectResponse({
+        callback:waitAsyncAndFail(c, "callback triggered instead of textOrRedirectHandler"),
+        exceptionHandler:waitAsyncAndFail(c, "exceptionHandler triggered instead of textOrRedirectHandler"),
+        errorHandler:waitAsyncAndFail(c, "errorHandler triggered instead of textOrRedirectHandler"),
+    });
+};
+
+window.testAdvancedRedirectResponse = function() {
+    var c = new dwrunit.SingleAsyncCompletor;
+    Test.testRedirectResponse({
+        callback:waitAsyncAndFail(c, "callback triggered instead of textOrRedirectHandler"),
+        exceptionHandler:waitAsyncAndFail(c, "exceptionHandler triggered instead of textOrRedirectHandler"),
+        errorHandler:waitAsyncAndFail(c, "errorHandler triggered instead of textOrRedirectHandler"),
+        textOrRedirectHandler:waitAsync(c, function(data) {
+          verifyEqual(302, data.status);
+        })
+    });
+}
 
 /*
  The textHtmlResponse tests only work on Jetty for now.  Comment them unless you need to run these tests and you are deployed to Jetty.
@@ -37,40 +60,6 @@ window.testAdvancedTextHtmlResponse = function() {
     });
     Test._path = oldPath; // by resetting the path immediately after sending we affect no other tests
 }*/
-
-window.testAdvancedGlobalTextHtmlResponse = function() {
-	var oldPath = Test._path;
-	Test._path = "/testdwr/textHtmlResponseTest.html";
-	var c = new dwrunit.SingleAsyncCompletor;
-	dwr.engine.setTextHtmlHandler(waitAsync(c, function(data) {
-        verifyEqual(200, data.status);
-        verifyTrue(data.responseText.indexOf("html") != -1);
-        verifyEqual("text/html", data.contentType);
-    }));
-	Test.doNothing({
-	    callback:waitAsyncAndFail(c, "callback triggered instead of textHtmlHandler."),
-	    exceptionHandler:waitAsyncAndFail(c, "exceptionHandler triggered instead of textHtmlHandler."),
-	    errorHandler:waitAsyncAndFail(c, "errorHandler triggered instead of textHtmlHandler.")
-	});
-	Test._path = oldPath; // by resetting the path immediately after sending we affect no other tests
-}
-
-window.testAdvancedTextHtmlResponse = function() {
-	var oldPath = Test._path;
-	Test._path = "/testdwr/textHtmlResponseTest.html";
-	var c = new dwrunit.SingleAsyncCompletor;
-	Test.doNothing({
-	    callback:waitAsyncAndFail(c, "callback triggered instead of textHtmlHandler."),
-	    exceptionHandler:waitAsyncAndFail(c, "exceptionHandler triggered instead of textHtmlHandler."),
-	    errorHandler:waitAsyncAndFail(c, "errorHandler triggered instead of textHtmlHandler."),
-	    textHtmlHandler:waitAsync(c, function(data) {
-	      verifyEqual(200, data.status);
-	      verifyTrue(data.responseText.indexOf("html") != -1);
-	      verifyEqual("text/html", data.contentType);
-	    })
-	});
-	Test._path = oldPath; // by resetting the path immediately after sending we affect no other tests
-}
 
 /**
  *
