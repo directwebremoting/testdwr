b7c80391c586c6c17e5ad36f3f89919b7cd25c77
mikewse <mikewse@users.noreply.github.com>
2014-08-24 00:38:59 +0000

    new test case for XSS attack

--- main/web/testVulnerabilities.js
+++ main/web/testVulnerabilities.js
@@ -2,11 +2,17 @@
 createTestGroup("Vulnerabilities");
 
 window.testVulnerabilitiesXssScriptTag = function() {
+    var c = new dwrunit.SingleAsyncCompletor;
+    window.youHaveBeenHackedFunc = waitAsync(c, function() {
+    	dwrunit.fail("YOU'VE BEEN HACKED");
+    	delete window.youHaveBeenHackedFunc;
+    });
     dwr.engine.beginBatch();
     dwr.engine._batch.fileUpload = true; // trigger iframe mode
-    var hackstr = "</script><script>alert('HACKED!');</script>";
-	Test.stringParam(hackstr, waitDwrCallbackOptions(function(retval) {
+    var hackstr = "</script><script>window.parent.youHaveBeenHackedFunc();</script>";
+	Test.stringParam(hackstr, waitAsync(c, function(retval) {
       verifyEqual(retval, hackstr);
+      delete window.youHaveBeenHackedFunc;
 	}));
     dwr.engine.endBatch();
 };
